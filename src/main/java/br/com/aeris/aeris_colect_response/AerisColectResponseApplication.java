646a82486b8bdb8c42502acd738f9cdc6dd36fe5
Maria Clara Fadel <mariaclarapereira@alunos.utfpr.edu.br>
2025-10-24 23:06:19 -0300

    initializr spring boot microservice

---
+++ src/main/java/br/com/aeris/aeris_colect_response/AerisColectResponseApplication.java
@@ -0,0 +1,13 @@
+package br.com.aeris.aeris_colect_response;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerisColectResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerisColectResponseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aeris/aeris_colect_response/AerisColectResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aeris.aeris_colect_response;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerisColectResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
